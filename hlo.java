3d15f50b37dd17e56fe9077b1db2ca847725dc15
vasanthsubramanian1028-hash <vasanthsubramanian1028@gmail.com>
2026-08-19 19:30:41 +0530

    this is just another file to test

---
+++ hlo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class hlo{
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+    }
+}
