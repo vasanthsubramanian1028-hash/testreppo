9c9875ad2d42e7d4a265e57aa24976578875bf5f
vasanthsubramanian1028-hash <vasanthsubramanian1028@gmail.com>
2026-08-19 19:34:45 +0530

    read and print three output from the input

--- hlo.java
+++ hlo.java
@@ -4,7 +4,7 @@ class hlo{
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
-        int c = scan.nextInt()
+        int c = scan.nextInt();
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
